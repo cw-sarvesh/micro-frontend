49448db2baaa399345024f4660115cda4c556592
Sarvesh Mishra <sarvesh.m@carwale.com>
2023-06-07 17:01:40 +0530

    update both app

--- cart/src/App.tsx
+++ cart/src/App.tsx
@@ -2,13 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.scss";
-
+// Importing micro frontend components
+import {Header } from "home/Header"
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: cart</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Tailwind</div>
+    <Header/>
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
--- home/webpack.config.js
+++ home/webpack.config.js
@@ -44,7 +44,9 @@ module.exports = (_, argv) => ({
       name: "home",
       filename: "remoteEntry.js",
       remotes: {},
-      exposes: {},
+      exposes: {
+        "./Header": "./src/Header.tsx"
+      },
       shared: {
         ...deps,
         react: {

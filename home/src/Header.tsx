5d773932c69b100f152a6cb45a4c509468192b3f
Sarvesh Mishra <sarvesh.m@carwale.com>
2023-06-07 12:26:34 +0530

    add initial files

---
+++ home/src/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header= ()=>{
+    return(
+        <div className="p-5 bg-blue-500 text-white text-3xl font-bold">
+            Hello from Header
+        </div>
+    );
+}
